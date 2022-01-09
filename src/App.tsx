166a1f3394370fc6dc046e061476cc114a1623f8
Yavuz Cirit <yavuzcirit23@gmail.com>
2022-01-09 14:41:34 +0300

    first commit after demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{FC,useState,ChangeEvent} from 'react';
 import './App.css';
+import {ITask} from './interfaces'
 
-function App() {
+const App:FC=()=>{
+  const [task,setTask]=useState<string>("");
+  const [deadline,setDeadline]=useState<number>(0);
+  const [todoList,setTodoList]=useState<ITask[]>([]);
+
+
+
+
+  const handleChange=(event:ChangeEvent<HTMLInputElement>):void=>{
+    if (event.target.name==="task"){
+    setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value))
+    }
+    
+  };
+
+const addTask=():void=>{
+  const newTask={taskName:task,deadline:deadline}
+  setTodoList([...todoList,newTask]);
+  console.log(todoList)
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <div className="header">
+          <div className='inputContainer'>
+            <input type="text" placeholder="Task..." name="task" onChange={handleChange}/>
+            <input type="text" placeholder="Deadline(in Days)..." name="deadline" onChange={handleChange}/>
+          </div>
+          <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList">
+
+      </div>
     </div>
   );
 }
